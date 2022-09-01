3cff5951676d175399dcea68dc22b292c44f0726
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-09-01 13:41:49 +0200

    Setup wp-env.

---
+++ pronamic-eu-vat-for-easy-digital-downloads.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Pronamic - EU VAT for Easy Digital Downloads
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-eu-vat-for-easy-digital-downloads/
+ * Description: This plugin extends the Easy Digital Downloads plugin with EU VAT support.
+ *
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-eu-vat-for-easy-digital-downloads
+ * Domain Path: /languages/
+ *
+ * License: GPL-2.0-or-later
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-give
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-2.0-or-later
+ */
